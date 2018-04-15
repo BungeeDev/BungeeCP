57628fd0e26d8ec12676bd4cb7fd45859f688dee
Alex Sobiek <asobiek881@gmail.com>
2018-04-14 19:40:19 -0500

    Auth Updates

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-﻿var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var config = require("./config.json");
-
-process.title = "BungeeCP Server";
+﻿process.title = "BungeeCP Server";
 console.log("BungeeCP Starting...");
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const config = require("./config.json");
+const bodyParser = require('body-parser');
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt-nodejs');
+const Schema = mongoose.Schema;
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.listen(config.port, function () {
     console.log("BungeeCP Started on Port " + config.port);
