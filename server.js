516cbb7b357b642973c1d51278f959040ca18382
Alex Sobiek <asobiek881@gmail.com>
2018-04-14 19:05:54 -0500

    Node Modules

---
+++ server.js
@@ -0,0 +1,12 @@
+﻿var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var config = require("./config.json");
+
+process.title = "BungeeCP Server";
+console.log("BungeeCP Starting...");
+
+app.listen(config.port, function () {
+    console.log("BungeeCP Started on Port " + config.port);
+});
